1a215739ff537a01094cb1a239cf675b4397be2b
nhom 1 <mylinh5627@gmail.com>
2024-12-30 13:33:26 +0700

    source

--- source_code/Server/Server/shutdown.cpp
+++ source_code/Server/Server/shutdown.cpp
@@ -1,50 +1,47 @@
 #include "shutdown.h"
-#include <thread>
-#include <chrono>
 
 bool ShutDownPC() {
     // Adjust privileges to enable shutdown
     HANDLE hToken;
     TOKEN_PRIVILEGES tkp;
+
     // Get a token for this process
     if (!OpenProcessToken(GetCurrentProcess(), TOKEN_ADJUST_PRIVILEGES | TOKEN_QUERY, &hToken)) {
-        std::cout << "Failed to get process token.\n";
+        std::cerr << "Failed to get process token.\n";
         return false;
     }
+
     // Get the LUID for the shutdown privilege
     if (!LookupPrivilegeValue(NULL, SE_SHUTDOWN_NAME, &tkp.Privileges[0].Luid)) {
-        std::cout << "Failed to lookup shutdown privilege.\n";
+        std::cerr << "Failed to lookup shutdown privilege.\n";
         CloseHandle(hToken);
         return false;
     }
+
     tkp.PrivilegeCount = 1;  // Set one privilege
     tkp.Privileges[0].Attributes = SE_PRIVILEGE_ENABLED;
+
     // Adjust the token privileges to enable shutdown
     if (!AdjustTokenPrivileges(hToken, FALSE, &tkp, 0, NULL, 0)) {
-        std::cout << "Failed to adjust token privileges.\n";
+        std::cerr << "Failed to adjust token privileges.\n";
         CloseHandle(hToken);
         return false;
     }
+
     // Check for any errors from AdjustTokenPrivileges
     if (GetLastError() == ERROR_NOT_ALL_ASSIGNED) {
-        std::cout << "The token does not have the required privileges.\n";
+        std::cerr << "The token does not have the required privileges.\n";
         CloseHandle(hToken);
         return false;
     }
-    std::cout << "System shutting down...\n";
 
-    if (!InitiateSystemShutdownEx(
-        NULL,                   // Target computer
-        NULL,        // Modifiable wide string
-        15,                      // Timeout in seconds
-        TRUE,                   // Force applications to close
-        TRUE,                  // Do not reboot after shutdown
-        SHTDN_REASON_MAJOR_OTHER | SHTDN_REASON_FLAG_PLANNED)) {
-        std::cout << "Failed to shutdown.\n";
+    // Shut down the system
+    if (!ExitWindowsEx(EWX_SHUTDOWN | EWX_FORCE, SHTDN_REASON_MAJOR_OTHER | SHTDN_REASON_FLAG_PLANNED)) {
+        std::cerr << "Failed to shutdown the system.\n";
         CloseHandle(hToken);
         return false;
     }
-    
+
     CloseHandle(hToken);
     return true;
 }
